dc6fe3c793bc71731991747b6d3fd36767617acc
Donosik <damian1003@onet.pl>
2021-04-09 21:15:06 +0200

    poprawa przekierowania z postów do restauracji

--- index.php
+++ index.php
@@ -15,25 +15,25 @@ HeaderDiv();
 <div class="wall">
     <div class="inside">
         <?php
-        $myFile="posts.txt";
-        $lines=file($myFile);
-        $i=0;
-        $j=0;
+        $myFile = "posts.txt";
+        $lines = file($myFile);
+        $i = 0;
+        $j = 0;
         foreach ($lines as $line)
         {
             $var = explode(':', $line, 2);
             $arr[$i] = $var[1];
             $i++;
-            if($i==6)
+            if ($i == 7)
             {
-                $i=0;
-                $posts[$j]=$arr;
+                $i = 0;
+                $posts[$j] = $arr;
                 $j++;
             }
         }
-        foreach($posts as $post)
+        foreach ($posts as $post)
         {
-            post($post[0],$post[1],$post[2],$post[3],$post[4],$post[5]);
+            post($post[0], $post[1], $post[2], $post[3], $post[4], $post[5], $post[6]);
         }
 
         ?>
@@ -44,13 +44,11 @@ hastagsDiv();
 ?>
 </div>
 <script>
-    var counter=document.getElementsByClassName("counter");
-    var like=document.getElementsByClassName("like");
+    var counter = document.getElementsByClassName("counter");
+    var like = document.getElementsByClassName("like");
 
-    for(let i=0;i<like.length;i++)
-    {
-        like[i].onclick=function()
-        {
+    for (let i = 0; i < like.length; i++) {
+        like[i].onclick = function () {
             counter[i].innerHTML++;
         }
     }
