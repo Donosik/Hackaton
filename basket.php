25158b766e8f2a4c89e35ad459e9d36ab93443e0
Donosik <damian1003@onet.pl>
2021-04-09 22:09:10 +0200

    koszyk formularz

--- basket.php
+++ basket.php
@@ -40,6 +40,38 @@ HeaderDiv();
 
                     }
                 }
+                echo '</div>';
+                ?>
+                <div style="width: 600px;margin: auto; clear: both;">
+                    <hr style="height: 2px;background-color: black;">
+                    <form method="post">
+                        <div class="basket_order">
+                            <a>First name</a><br/>
+                            <a>Second name</a><br/>
+                            <a>E-mail</a><br/>
+                            <a>City</a><br/>
+                            <a>Your address:</a><br/>
+                            <a>Phone</a><br/>
+                            <a>When do you want to get your food?</a><br/>
+                        </div>
+                        <div style="float: left">
+                            <input type="text" name="firstName" placeholder="First name"><br/>
+
+                            <input type="text" name="secondName" placeholder="Second name"><br/>
+
+                            <input type="text" name="e-mail" placeholder="E-mail"><br/>
+
+                            <input type="text" name="city" placeholder="City"><br/>
+
+                            <input type="text" name="address" placeholder="Your address:"><br/>
+
+                            <input type="text" name="phone" placeholder="Phone"><br/>
+
+                            <input type="text" name="time" placeholder="When do you want to get your food?"><br/>
+                        </div>
+                    </form>
+                </div>
+                <?php
             }
             else
             {
